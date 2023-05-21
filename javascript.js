d49d0ffe0a402cc2a0005b334650c607c961f150
Steve Richards <steverichards80@hotmail.com>
2023-05-21 17:11:59 +0100

    Correct grid initialised using JS

--- javascript.js
+++ javascript.js
@@ -3,6 +3,18 @@
 const container1 = document.querySelector('.container1');
 
 
-const score = document.createElement('');
+for (let i = 1; i < 257; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.cssText = "border: 1px solid black; height: 50px; width: 50px";  
+    square.addEventListener("mouseover", changeColour);
+    container1.appendChild(square);
+  }
+ const squares = document.getElementsByClassName("square");
 
-container1.appendChild();
+  function changeColour() {
+    squares.style.backgroundColor = "red";
+  }
+
+ squares.addEventListener("mouseover", changeColour);
+
